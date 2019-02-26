8a16148df6605585d18dfe72718e736898b0b333
Angular <vovabarneckyi@gmail.com>
2019-02-26 15:03:33 +0200

    Header component

--- medicine-centre/src/app/app.module.ts
+++ medicine-centre/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [
+    HeaderComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ medicine-centre/src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
